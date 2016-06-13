9cf6edf6c73208fff0a9905de3c993ff97a681c9
cezarsantanna <cezar.santanna@gmail.com>
2016-06-13 20:10:15 -0300

    Version 0.0.2
    Insert your data inside the file data.csv, following the format
    lat_src,long_src;lat_dst,long_dst
    run with python get_distance.py the result will be save in the file
    result.csv following the format
    lat_src,long_src;lat_dst,long_dst;distance

--- get_distance.py
+++ get_distance.py
@@ -6,16 +6,24 @@ import urllib
 for line in open('API_Key.txt', 'r'):
     API_Key = line
 
-with open('data.csv', 'rb') as csvfile:
-    reader = csv.reader(
-        csvfile, delimiter=';')
-    for row in reader:
-        orig_coord = row[0]
-        dest_coord = row[1]
-        url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins={0}\
-              &destinations={1}&key={2}&mode=driving&language=en-EN\
-              &units=metric".format(str(orig_coord), str(dest_coord), str(API_Key))
-        result = simplejson.load(urllib.urlopen(url))
-        driving_distance = result['rows'][0][
-            'elements'][0]['distance']['value']
-        print driving_distance
+with open('data.csv', 'rb') as rcsvfile:
+    with open('result.csv', 'wb') as wcsvfile:
+        reader = csv.reader(
+            rcsvfile,
+            delimiter=';')
+        writer = csv.writer(wcsvfile,
+                            delimiter=';',
+                            quotechar='|',
+                            quoting=csv.QUOTE_MINIMAL)
+        for row in reader:
+            orig_coord = row[0]
+            dest_coord = row[1]
+            url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins={0}\
+                  &destinations={1}&key={2}&mode=driving&language=en-EN\
+                  &units=metric".format(str(orig_coord),
+                                        str(dest_coord),
+                                        str(API_Key))
+            result = simplejson.load(urllib.urlopen(url))
+            driving_distance = result['rows'][0][
+                'elements'][0]['distance']['value']
+            writer.writerow([orig_coord, dest_coord, driving_distance])
